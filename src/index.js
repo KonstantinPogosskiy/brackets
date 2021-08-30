695c8a61664b8854ebe9519d072c84ed9c7ec4b6
KonstantinPogosskiy <darcus666@yandex.ru>
2021-08-30 23:16:24 +0300

    refactor: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let open = [];
+    let close = [];
+    let closeIndex;
+    let openIndex;
+    bracketsConfig.forEach(item => {
+        open.push(item[0]);
+        close.push(item[1]);
+    })
+
+    let arrayOfBrackets = str.split('');
+    for (let i = 0; i < arrayOfBrackets.length; i++) {
+        openIndex = open.indexOf(arrayOfBrackets[i]);
+        closeIndex = close.indexOf(arrayOfBrackets[i]);
+        if ((openIndex === -1) && (closeIndex === -1)) return false;
+
+        if ((stack.length === 0) && (openIndex !== closeIndex && closeIndex !== -1)) return false;
+
+        if (closeIndex !== -1 && stack.includes(closeIndex)) {
+            if (closeIndex === stack[stack.length - 1]) stack.pop(); else return false;
+
+        } else if (openIndex !== -1 && (openIndex !== closeIndex || !stack.includes(openIndex))) {
+            stack.push(openIndex);
+        }
+    }
+    if(stack.length > 0) return false;
+
+    return true;
 }
